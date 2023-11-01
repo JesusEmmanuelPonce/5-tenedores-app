af274953050cc9c8e3c35811564fe5efe4f7b5bb
JesusEmmanuelPonce <j.emmanuel.pb@hotmail.com>
2023-10-31 19:46:02 -0600

    feat: added dependencies

---
+++ src/screen/Ranking/Ranking.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native';
+
+const Ranking = () => {
+    return (
+        <View>
+            <Text>Rankings</Text>
+        </View>
+    )
+}
+
+export default Ranking
